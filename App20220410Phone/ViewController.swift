7b549e45ac4448291274d63781d6ccdb76e9fc4b
Huang Grace <huang.yahui.grace@gmail.com>
2022-04-10 15:10:26 +0800

    簡訊認證

--- App20220410Phone/ViewController.swift
+++ App20220410Phone/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myPhoneNo: UITextField!
+    
+    var varID = "AJOnW4SCux1UAGuhIlvigO57vTOWLUU5vzntMgx9AQHIus1w5kNYyg1wT79lWfQPr_ywB0vRD-SAjWRq69csQELbNY7x7YdqHqsy85cN0XBGVHc9dnl7gR8WYfUcLoFGPN1mpGCAZfcdVxdgAH0OsWgfoU_lvIKk9HfMoBXAMD_mYcjvQMMWg1ji2w_fOXA1nA05LYKgk0lPVQ70hUhZ0as17xdFc1S60iL0VWZY8cYW3N_LBI7IITg"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getAuthCode(_ sender: Any) {
+        let phoneNum = myPhoneNo.text ?? ""
+        
+        // PhoneNumber Format
+        
+        PhoneAuthProvider.provider()
+          .verifyPhoneNumber(phoneNum, uiDelegate: nil) { verificationID, error in
+              if let error = error {
+//                self.showMessagePrompt(error.localizedDescription)
+                  print("error:\(error.localizedDescription)")
+                return
+              }
+              // Sign in using the verificationID and the code sent to the user
+              print("verificationID: \(String(describing: verificationID))")
+          }
+        
+    }
+    
 }
 
