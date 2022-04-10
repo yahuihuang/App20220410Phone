6167ee4c71ee4dcf8b4c98e2b2ce6fe429472959
Huang Grace <huang.yahui.grace@gmail.com>
2022-04-10 15:29:30 +0800

    發送簡訊認證

--- App20220410Phone/ViewController.swift
+++ App20220410Phone/ViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    // Format: +886921XXXXXX
     @IBOutlet weak var myPhoneNo: UITextField!
     
-    var varID = "AJOnW4SCux1UAGuhIlvigO57vTOWLUU5vzntMgx9AQHIus1w5kNYyg1wT79lWfQPr_ywB0vRD-SAjWRq69csQELbNY7x7YdqHqsy85cN0XBGVHc9dnl7gR8WYfUcLoFGPN1mpGCAZfcdVxdgAH0OsWgfoU_lvIKk9HfMoBXAMD_mYcjvQMMWg1ji2w_fOXA1nA05LYKgk0lPVQ70hUhZ0as17xdFc1S60iL0VWZY8cYW3N_LBI7IITg"
+    @IBOutlet weak var myAuthCode: UITextField!
+    
+    var verID = "AJOnW4SCux1UAGuhIlvigO57vTOWLUU5vzntMgx9AQHIus1w5kNYyg1wT79lWfQPr_ywB0vRD-SAjWRq69csQELbNY7x7YdqHqsy85cN0XBGVHc9dnl7gR8WYfUcLoFGPN1mpGCAZfcdVxdgAH0OsWgfoU_lvIKk9HfMoBXAMD_mYcjvQMMWg1ji2w_fOXA1nA05LYKgk0lPVQ70hUhZ0as17xdFc1S60iL0VWZY8cYW3N_LBI7IITg"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                // 已登入
+                print("phone: \(String(describing: user.phoneNumber) )")
+            } else {
+                // 未登入
+                print("not login !")
+            }
+        }
     }
 
     @IBAction func getAuthCode(_ sender: Any) {
@@ -32,9 +45,18 @@ class ViewController: UIViewController {
               }
               // Sign in using the verificationID and the code sent to the user
               print("verificationID: \(String(describing: verificationID))")
+              self.verID = verificationID ?? ""
           }
         
     }
     
+    @IBAction func goLogin(_ sender: Any) {
+        let code = myAuthCode.text ?? ""
+        let credential = PhoneAuthProvider.provider().credential(
+          withVerificationID: self.verID,
+          verificationCode: code
+        )
+        Auth.auth().signIn(with: credential)
+    }
 }
 
