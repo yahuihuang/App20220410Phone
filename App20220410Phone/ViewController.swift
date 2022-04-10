5a1bc13a92ef4b628d46a07a9f4b182e69151266
Huang Grace <huang.yahui.grace@gmail.com>
2022-04-10 16:04:34 +0800

    顯示Button檢查

--- App20220410Phone/ViewController.swift
+++ App20220410Phone/ViewController.swift
@@ -11,8 +11,9 @@ import Firebase
 class ViewController: UIViewController {
     // Format: +886921XXXXXX
     @IBOutlet weak var myPhoneNo: UITextField!
-    
+    @IBOutlet weak var myAuthBtn: UIButton!
     @IBOutlet weak var myAuthCode: UITextField!
+    @IBOutlet weak var mySignonBtn: UIButton!
     
     var verID = "AJOnW4SCux1UAGuhIlvigO57vTOWLUU5vzntMgx9AQHIus1w5kNYyg1wT79lWfQPr_ywB0vRD-SAjWRq69csQELbNY7x7YdqHqsy85cN0XBGVHc9dnl7gR8WYfUcLoFGPN1mpGCAZfcdVxdgAH0OsWgfoU_lvIKk9HfMoBXAMD_mYcjvQMMWg1ji2w_fOXA1nA05LYKgk0lPVQ70hUhZ0as17xdFc1S60iL0VWZY8cYW3N_LBI7IITg"
     
@@ -24,9 +25,19 @@ class ViewController: UIViewController {
             if let user = user {
                 // 已登入
                 print("phone: \(String(describing: user.phoneNumber) )")
+                
+                self.myPhoneNo.isEnabled = false
+                self.myAuthCode.isEnabled = false
+                self.myAuthBtn.isEnabled = false
+                self.mySignonBtn.isEnabled = false
             } else {
                 // 未登入
                 print("not login !")
+                
+                self.myPhoneNo.isEnabled = true
+                self.myAuthCode.isEnabled = false
+                self.myAuthBtn.isEnabled = true
+                self.mySignonBtn.isEnabled = false
             }
         }
     }
@@ -46,6 +57,9 @@ class ViewController: UIViewController {
               // Sign in using the verificationID and the code sent to the user
               print("verificationID: \(String(describing: verificationID))")
               self.verID = verificationID ?? ""
+              
+              self.myAuthCode.isEnabled = true
+              self.mySignonBtn.isEnabled = true
           }
         
     }
@@ -57,6 +71,19 @@ class ViewController: UIViewController {
           verificationCode: code
         )
         Auth.auth().signIn(with: credential)
+    }
+    
+    @IBAction func goLogout(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Logout Error")
+        }
+    }
+    
+    // MARK: keyboard close
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
 
